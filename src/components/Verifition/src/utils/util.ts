68514da7cbb5e757e52eae08b849516c98b5dd52
DESKTOP-7TP68J3\PC <824314051@qq.com>
2024-09-10 14:38:41 +0800

    滑块验证码样式布局修复

--- src/components/Verifition/src/utils/util.ts
+++ src/components/Verifition/src/utils/util.ts
@@ -5,9 +5,17 @@ export function resetSize(vm) {
     vm.$el.parentNode.offsetWidth || EmployeeWindow.offsetWidth;
   const parentHeight =
     vm.$el.parentNode.offsetHeight || EmployeeWindow.offsetHeight;
-  img_width = EmployeeWindow.innerWidth - 20;
-  img_height = img_width / 2 + "px";
-  img_width = img_width + "px";
+  if (vm.imgSize.width.indexOf("%") != -1) {
+    img_width = (parseInt(vm.imgSize.width) / 100) * parentWidth + "px";
+  } else {
+    img_width = vm.imgSize.width;
+  }
+
+  if (vm.imgSize.height.indexOf("%") != -1) {
+    img_height = (parseInt(vm.imgSize.height) / 100) * parentHeight + "px";
+  } else {
+    img_height = vm.imgSize.height;
+  }
 
   if (vm.barSize.width.indexOf("%") != -1) {
     bar_width = (parseInt(vm.barSize.width) / 100) * parentWidth + "px";
