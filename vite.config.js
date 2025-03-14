6e99e38e53c6cca349cb0672d77d47c1f1e4b7e4
DESKTOP-7TP68J3\PC <824314051@qq.com>
2025-03-14 17:56:11 +0800

    update:tauri config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 export default defineConfig({
   // plugins: [vue(), basicSsl()],
   plugins: [vue()],
+  // 防止 Vite 清除 Rust 显示的错误
+  clearScreen: false,
+  // 添加有关当前构建目标的额外前缀，使这些 CLI 设置的 Tauri 环境变量可以在客户端代码中访问
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
   optimizeDeps: {
     include: ["mitt"],
   },
@@ -33,5 +37,14 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600,
     outDir: 'dist',
+    // Tauri 在 Windows 上使用 Chromium，在 macOS 和 Linux 上使用 WebKit
+    target:
+        process.env.TAURI_ENV_PLATFORM === 'windows'
+            ? 'chrome105'
+            : 'safari13',
+    // 在 debug 构建中不使用 minify
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // 在 debug 构建中生成 sourcemap
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
 });
